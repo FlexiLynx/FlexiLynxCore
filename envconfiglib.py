233d632133b1fbbf79a2e51b768714d03429ad5d
Shae <a.thomas.vc@gmail.com>
2023-12-31 16:38:28 -0800

    Made the styling slightly more consistent with the rest of the project

--- envconfiglib.py
+++ envconfiglib.py
@@ -15,6 +15,7 @@ __all__ = ('EnvConfig',)
 class EnvConfig(UserDict):
     'A read-only, case-insensitive namespace/dictionary that is populated by an environment variable or .flynx.env file'
     __slots__ = ('_envvar', '_type', '_rawenv', '_is_frozen')
+
     def __init__(self, var: str, type_: type | typing.Callable[[str], typing.Any] = literal_eval, freeze: bool = True):
         self._envvar = var
         self._type = type_
