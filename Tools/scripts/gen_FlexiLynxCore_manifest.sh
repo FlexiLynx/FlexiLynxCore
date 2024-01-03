e3fcb59f6106dc89d0bd71a42c24cd6dfdb7ad67
Shae <a.thomas.vc@gmail.com>
2024-01-02 21:16:58 -0800

    Changed naming scheme of ManifestStore

--- Tools/scripts/gen_FlexiLynxCore_manifest.sh
+++ Tools/scripts/gen_FlexiLynxCore_manifest.sh
@@ -3,10 +3,10 @@
 ${PYTHON:-} ./Tools/manifestutils.py gen manifest 'flexilynx.core' \
     'FlexiLynx Core' --desc 'Core files required to run FlexiLynx, including frameworks for manifests, modules, and plugins' \
     'Shae VanCleave' --contact 'https://github.com/Tiger-Tom' \
-    'https://raw.githubusercontent.com/FlexiLynx/ManifestStore/main/FlexiLynx/Core.manifest.ini' \
+    'https://raw.githubusercontent.com/FlexiLynx/ManifestStore/main/flexilynx/core.ini' \
     'https://raw.githubusercontent.com/FlexiLynx/FlexiLynxCore/main/' \
     --default-root . --include '**/*.py' --exclude 'Tools/*' \
     --sign ../shae.pyk \
     --min-version 3 12 0 \
     --meta-version "commit $(git rev-parse --short HEAD)" \
-    --output '../ManifestStore/FlexiLynx/Core.manifest.ini'
+    --output '../ManifestStore/flexilynx/core.ini'
--- Tools/scripts/update_FlexiLynxCore_manifest.sh
+++ Tools/scripts/update_FlexiLynxCore_manifest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-${PYTHON:-} ./Tools/manifestutils.py mod update ../ManifestStore/FlexiLynx/Core.manifest.ini \
+${PYTHON:-} ./Tools/manifestutils.py mod update ../ManifestStore/flexilynx/core.ini \
     --default-root . --include '**/*.py' --exclude 'Tools/*' \
     --sign ../shae.pyk \
     --meta-version "commit $(git rev-parse --short HEAD)"
