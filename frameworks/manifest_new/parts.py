1c39cffe03f9611abb080de8a4043b7ccb469065
Shae <a.thomas.vc@gmail.com>
2024-01-06 18:01:11 -0800

    Implemented exporting as a `p_export()` method in part classes

--- frameworks/manifest_new/parts.py
+++ frameworks/manifest_new/parts.py
@@ -12,12 +12,93 @@ import types
 import typing
 from functools import partial
 from dataclasses import dataclass, field, asdict
+from collections import abc as cabc
+from collections.abc import Iterable, Iterator, Set, Mapping
 #</Imports
 
 #> Header >/
 # Parts base
 class BasePart:
-    '''A no-op class that serves to only tie manifest parts together and for `isinstance()` checks'''
+    '''A base class for all manifest parts'''
+
+    @classmethod
+    def _p_export_val(cls, v: typing.Any) -> bool | int | float | complex | bytes | str | tuple | frozenset | types.MappingProxyType | None:
+        '''
+            Exports values into immutable primitives and/or collections in the following order:
+                (note that the number next to each item groups items where order is non-important in an implementation
+                (for instance, in any implementation, handing exporting to `v.[_]p_export()` should always be done first)
+             1) Hands the work of exporting over to `v._p_export()` or `v.p_export()` if `v` has either of those methods
+             2) Returns (non-`bool`) constants (`None`, `NotImplemented`, and `Ellipsis`/`...`) directly
+             2) Returns primitives (`bool`, `int`, `float`, `complex`, `str`, and `bytes`)
+                    Primitives are first passed to their own constructor to reduce subclasses to an immutable state
+                    Note that `bytearray` is also captured, and is reduced to `bytes`
+                    Note that `bool` should be checked before `int`, as `bool` is a subclass of `int`
+             3) Hands over mappings (`collections.abc.Mapping`) to `_p_export_dict()`
+             3) Hands over sequences (`collections.abc.Sequence`) to `_p_export_iterable()` and converts it to a `tuple`
+             3) Hands over sets (`collections.abc.Set`) to `_p_export_iterable()` and converts it to a `frozenset`
+             4) Tries to `return _p_export_unknown(v)`, if it exists (and is not `None`), so that subclasses can add that method
+                    `_p_export_unknown()` should raise `TypeError` if it can't deal with the type
+            If an unknown value is encountered, then it is handed to `_p_export_unknown()` (if it exists and is not `None`)
+                If `_p_export_unknown()` cannot handle the type, or is not set, then `TypeError` is raised
+                    (`_p_export_unknown()` should raise the `TypeError` at its own discretion)
+        '''
+        if (sfunc := (getattr(v, '_p_export', None) or getattr(v, 'p_export', None))) is not None:
+            return sfunc()
+        match v:
+            # return constants directly
+            ## types are used in the pattern below because NotImplemented and Ellipsis are seen as names
+            case (None | types.NotImplementedType() | types.EllipsisType()): return v
+            # return primitives, ensure that they are reduced to primitives in cases of subclasses
+            case bool(): return bool(v)
+            case int(): return int(v)
+            case float(): return float(v)
+            case complex(): return complex(v)
+            case bytes() | bytearray(): return bytes(v)
+            case str(): return str(v)
+            # handle mappings and sequences
+            case cabc.Mapping():
+                return types.MappingProxyType(dict(cls._p_export_dict(v)))
+            case cabc.Sequence():
+                return tuple(cls._p_export_iterable(v))
+            case cabc.Set():
+                return frozenset(cls._p_export_iterable(v))
+        if (ufunc := getattr(cls, '_p_export_unknown', None)) is not None:
+            try: return ufunc(v)
+            except TypeError as e:
+                raise TypeError(f'Cannot export unknown type {type(v).__qualname__!r} of value {v!r}: _p_export_unknown() raised {e!r}')
+        raise TypeError(f'Cannot export unknown type {type(v).__qualname__!r} of value {v!r} (and _p_export_unknown is not defined)')
+    @classmethod
+    def _p_export_iterable(cls, v: typing.Iterable) -> typing.Iterable:
+        '''
+            Exports an iterable using `_p_export_val()` on each value
+            Note that an assertion is made to prevent iterators themselves from being exported,
+                as exporting a partially completed iterator could cause hard-to-find issues
+        '''
+        assert not isinstance(v, cabc.Iterator), 'Refusing to export an iterator as it may lead to unpredictable results'
+        return (cls._p_export_val(vv) for vv in v)
+    @classmethod
+    def _p_export_dict(cls, d: dict) -> typing.Iterable[tuple[str, bool | int | float | complex | bytes | str | tuple | frozenset | types.MappingProxyType | None]]:
+        '''
+            Exports a dictionary into an iterable of `(key, _p_export_val(val))` tuples
+                Ignores all keys that start with `P_`, `p_`, or `_`
+        '''
+        return ((k, cls._p_export_val(v)) for k,v in d.items())
+    def p_export(self) -> types.MappingProxyType[str, [bool | int | float | complex | bytes | str | tuple | frozenset | types.MappingProxyType | None]]:
+        '''
+            Converts this part into a dictionary (`mappingproxy`) of primitive and immutable types
+            Values beginning with `p_`, `P_`, or `_` should not be exported
+            If a value has a `._p_export()` or `.p_export()` method, that should be used to export the object
+                No further processing should be done on that value
+                `.p_export()` should be used to export a part directly, `._p_export()` is to override exporting specifically for nested exports
+            If a subclass of `BasePart` wants to only modify exporting of a specific *type* of value, then the `_p_export_val()` classmethod can be overridden
+                It is recommended to `return super()._p_export_val(v)` on types not recognized by the overriden method
+                    or at the very least `return mappingproxy(dict(v.p_export()))` or `return mappingproxy(dict(v.p_export()))` if `v` has either of those methods,
+                        `return mappingproxy(dict(_p_export_dict(v)))` for mappings (`mappingproxy` is `types.MappingProxyType`),
+                        `return frozenset(_p_export_iterable(v))` for sets,
+                        and `return tuple(_p_export_iterable(v))` for otherwise unhandled sequences
+            Note that structured parts should have immutable values anyway (`dict`s are an exception as there is no built-in frozen version)
+        '''
+        return types.MappingProxyType(dict(self._p_export_dict(asdict(self))))
 class UnstructuredBasePart[*ContentTypes](BasePart):
     '''A base part for allowing a manifest part to contain variable data'''
     def __init__(self, **kwargs: typing.Union[*ContentTypes]):
