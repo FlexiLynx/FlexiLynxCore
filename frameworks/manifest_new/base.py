a645382a1d88723a230745556079862379869844
Shae <a.thomas.vc@gmail.com>
2024-01-08 11:28:35 -0800

    Decoupled `ManifestType` from `[_]PartUnion_Hybrid`

--- frameworks/manifest_new/base.py
+++ frameworks/manifest_new/base.py
@@ -12,19 +12,21 @@ import FlexiLynx
 __all__ = ('ManifestType',)
 logger = FlexiLynx.logger.getChild('core.fw.manif')
 
-class _ManifestType(base._PartUnion_Hybrid):
+class _ManifestType:
     '''
         A base class for all manifests
 
-        Includes `IDManifestPart` and `CryptManifestPart` (from `CoreManifestParts`)
+        Includes `IDManifestPart` and `CryptManifestPart`
     '''
     __slots__ = ()
     m_types = {}
 
-    def m_register(self, name: str):
-        assert name not in self.m_types, f'{name!r} was already registered by {self.m_types[name].__qualname__}'
-        assert cls not in self.m_types.values(), f'{self.__qualname__} was already registered under name(s) {", ".join((n for n,c in self.m_types.items() if c is self))}'
-        self.m_types[name] = self
+    @classmethod
+    def m_register(cls):
+        '''Registers this manifest class under its `__name__` to allow for later import-discovery'''
+        assert cls.__name__ not in cls.m_types, f'{cls.__name__!r} was already registered by {cls.m_types[cls].__qualname__}'
+        assert cls not in cls.m_types.values(), f'{cls.__name__} ({cls.__qualname__}) was already registered under name(s) {", ".join((n for n,c in cls.m_types.items() if c is cls))}'
+        cls.m_types[cls.__name__] = cls
     @classmethod
     def m_from_map(cls, m: typing.Mapping) -> typing.Self:
         '''Converts a mapping to a manifest, guessing the type with `m["type"]`'''
@@ -32,9 +34,15 @@ class _ManifestType(base._PartUnion_Hybrid):
             return c(**m['type'])
         raise TypeError(f'Unknown manifest type {m["type"]!r}')
 class _ManifestTypeMeta(base._PartUnion_HybridMeta):
-    def __call__(cls, m_name: str, *, p_defaults: typing.Mapping[str, base.BasePart] = {}, **parts: base.BasePart) -> type[_ManifestType]:
-        if not parts: return type(m_name, (CoreManifestParts, _ManifestType,), {})
-        return super().__call__(m_name, *CoreManifestParts.p_struct_cls, _bases=(_ManifestType,), **parts)
+    def __call__(cls, m_name: str, *, p_defaults: typing.Mapping[str, base.BasePart] = {}, m_register: bool = True, **parts: base.BasePart) -> type[_ManifestType]:
+        namespace = {'type': m_name, 'm_parts': parts}
+        c = super().__call__(m_name, *CoreManifestParts.p_struct_cls, _bases=(_ManifestType,), _namespace=namespace, **parts) \
+            if parts else type(m_name, (CoreManifestParts, _ManifestType,), namespace)
+        if m_settype: c.type = m_name
+        if m_register:
+            assert m_settype, 'Cannot register without setting type'
+            c.m_register()
+        return c
     def __instancecheck__(cls, other: typing.Any) -> bool:
         return isinstance(other, _ManifestType)
     def __subclasscheck__(cls, other: type) -> bool:
@@ -44,5 +52,4 @@ class ManifestType(metaclass=_ManifestTypeMeta):
     __doc__ = _ManifestType.__doc__
 
     m_types = _ManifestType.m_types
-    m_register = _ManifestType.m_register
     m_from_map = _ManifestType.m_from_map
