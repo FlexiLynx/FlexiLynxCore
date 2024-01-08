22c65109c44e8942b47c56b8f153c5ca70f2f204
Shae <a.thomas.vc@gmail.com>
2024-01-08 12:00:55 -0800

    Added `filter_keys()` function

--- libraries/util.py
+++ libraries/util.py
@@ -8,7 +8,7 @@ from collections import abc as cabc
 #</Imports
 
 #> Header >/
-__all__ = ('concat_mappings', 'dictdir', 'map_vals',
+__all__ = ('concat_mappings', 'dictdir', 'filter_keys', 'map_vals',
            'sequential')
 
 # Mapping utilities
@@ -19,6 +19,10 @@ def dictdir(o: typing.Any) -> dict[str, typing.Any]:
     '''Gets an object's keys and values from its `__dict__` and `__slots__` attributes'''
     return {a: getattr(o, a) for a in getattr(o, '__slots__')} \
            | getattr(o, '__dict__', {})
+def filter_keys(func: typing.Callable[[typing.Any], bool], map_: typing.Mapping,
+                type_: type[typing.Mapping] | typing.Callable[[tuple[tuple[typing.Any, typing.Any], ...]], typing.Any] = dict) -> typing.Mapping | typing.Any:
+    '''Returns a dictionary composed of `map_`, but with all keys where `func(key)` returns false removed'''
+    return type_((k, v) for k,v in map_.items() if func(k))
 def map_vals(func: typing.Callable[[typing.Any], typing.Any], *maps: typing.Mapping,
              type_: type[typing.Mapping] | typing.Callable[[tuple[tuple[typing.Any, typing.Any], ...]], typing.Any] = dict) -> typing.Mapping | typing.Any:
     '''Returns a dictionary composed of all of `maps`, where each value has been fed through `func`'''
