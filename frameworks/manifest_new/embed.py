cc7621af79f4d96f9558474163ecedfa981a7abf
Shae <a.thomas.vc@gmail.com>
2024-01-08 09:06:40 -0800

    Partially implemented manifest embedding

--- frameworks/manifest_new/embed.py
+++ frameworks/manifest_new/embed.py
@@ -13,6 +13,8 @@
 #> Imports
 import re
 
+from .base import Manifest
+
 from FlexiLynx.core import encodings, packlib
 #</Imports
 
@@ -20,30 +22,32 @@ from FlexiLynx.core import encodings, packlib
 __all__ = ('embed_manifest', 'strip_manifest', 'extract_manifest')
 
 encoding = 'b85'
-embed_str = '__EMBEDDED_MANIFEST__ = {}'
-embed_patt = re.compile(fr'''^{embed_str.format(f"""'({encodings.char_patts["b85"].pattern}+)'""")}(?:\n|$)''')
-def embed_manifest(contents: str, man: 'Manifest') -> str:
+embed_str = "__EMBEDDED_MANIFEST__ = ('{type}', '{pack}')"
+pack_patt = f'(?P<pack>{encodings.char_patts["b85"].pattern}+)'
+type_patt = r'(?P<type>[!#-&(-\[\]-~])'
+embed_patt = re.compile(fr'''^{embed_str.format(type=type_patt, pack=pack_patt)}(?:\n|$)''')
+def embed_manifest(contents: str, man: Manifest, type_key: str | None = None) -> str:
     '''Embeds a manifest object into a Python file'''
-    raise NotImplementedError
-def embed_manifest_dict(contents: str, man: dict) -> str:
+    return embed_manifest_dict(contents, man.p_export(), man.type if type_key is None else type_key)
+def embed_manifest_dict(contents: str, man: dict, type_key: str) -> str:
     '''Embeds a dictionary into a Python file'''
-    return embed_packed_manifest_dict(contents, packlib.pack(man))
-def embed_packed_manifest_dict(contents: str, packed: bytes) -> str:
+    return embed_packed_manifest_dict(contents, packlib.pack(man), type_key)
+def embed_packed_manifest_dict(contents: str, packed: bytes, type_key: str) -> str:
     '''Embeds a packed manifest dict into a Python file'''
-    return f'{embed_str.format(repr(encodings.encode(encoding, packed)))}\n{strip_manifest(contents)}'
+    return f'{embed_str.format(type=type_key, pack=encodings.encode(encoding, packed))}\n{strip_manifest(contents)}'
 def strip_manifest(contents: str) -> str:
     '''Strips an embedded manifest rom a Python file'''
     return embed_patt.sub('', contents)
-def extract_manifest(contents: str) -> None:
+def extract_manifest(contents: str) -> Manifest | None:
     '''Extracts an embedded manifest from a Python file'''
     raise NotImplementedError
-def extract_manifest_dict(contents: str) -> dict | None:
-    '''Extracts a manifest dictionary from a Python file'''
+def extract_manifest_dict(contents: str) -> tuple[str, dict] | None:
+    '''Extracts a manifest type-key and manifest dictionary from a Python file'''
     if (p := extract_packed_manifest_dict(contents)) is not None:
         return packlib.unpack(p)[0]
     return None
-def extract_packed_manifest_dict(contents: str) -> bytes | None:
-    '''Extracts a packed manifest dictionary from a Python file'''
+def extract_packed_manifest_dict(contents: str) -> tuple[str, bytes] | None:
+    '''Extracts a manifest type-key and packed manifest dictionary and from a Python file'''
     if m := embed_patt.match(contents):
         return encodings.decode(encoding, m.group(1))
     return None
