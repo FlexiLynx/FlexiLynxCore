16ad4698dfdec40b69930d1b74c2a38813f9d24c
Shae <a.thomas.vc@gmail.com>
2024-01-07 18:48:03 -0800

    Added `sequential()` function

--- libraries/util.py
+++ libraries/util.py
@@ -8,8 +8,10 @@ from collections import abc as cabc
 #</Imports
 
 #> Header >/
-__all__ = ('concat_mappings', 'dictdir', 'map_vals')
+__all__ = ('concat_mappings', 'dictdir', 'map_vals',
+           'sequential')
 
+# Mapping utilities
 def concat_mappings(*maps: typing.Mapping, type_: type[typing.Mapping] | typing.Callable[[tuple[tuple[typing.Any, typing.Any], ...]], typing.Any] = dict) -> typing.Mapping | typing.Any:
     '''Concatenates multiple mappings into a single one'''
     return type_(sum(map(tuple, map(cabc.ItemsView, maps)), start=()))
@@ -21,3 +23,18 @@ def map_vals(func: typing.Callable[[typing.Any], typing.Any], *maps: typing.Mapp
              type_: type[typing.Mapping] | typing.Callable[[tuple[tuple[typing.Any, typing.Any], ...]], typing.Any] = dict) -> typing.Mapping | typing.Any:
     '''Returns a dictionary composed of all of `maps`, where each value has been fed through `func`'''
     return concat_mappings(*({k: func(v) for k,v in m.items()} for m in maps), type_=type_)
+# Sequence utilities
+def sequential(seq: typing.Iterable[int]) -> typing.Iterator[tuple[int, ...]]:
+    '''Yields tuples of sequential integers in ascending order'''
+    if not seq: return
+    seq = sorted(seq)
+    working = [seq[0]]
+    if len(seq) < 2:
+        yield tuple(working)
+        return
+    for n in seq[1:]:
+        if n-1 != working[-1]:
+            yield tuple(working)
+            working = [n]
+        else: working.append(n)
+    yield tuple(working)
