ff5ede902c30287c3657fcb79ddcb9bcad447ba8
Shae <a.thomas.vc@gmail.com>
2024-01-21 14:19:20 -0800

    Added some simple/common manifest types

--- frameworks/manifest_new/__init__.py
+++ frameworks/manifest_new/__init__.py
@@ -1,11 +1,20 @@
 #!/bin/python3
 
-#> Package >/
-__all__ = ('Manifest', 'base', 'crypt', 'exceptions', 'file', 'parts')
+__all__ = ('Manifest', 'base', 'crypt', 'exceptions', 'file', 'parts',
+           'BareContentManifest', 'ContentManifest')
 
+#> Package
 from base import Manifest
 from . import base
 from . import crypt
 from . import exceptions
 from . import file
 from . import parts
+#</Package
+
+#> Header >/
+BareContentManifest = base.ManifestType('BareContentManifest',
+    content=parts.ContentManifestPart)
+ContentManifest = base.ManifestType('ContentManifest', BareContentManifest,
+    metadata=parts.MetadataManifestPart,
+    cascade=parts.extended.KeyCascadePart)
