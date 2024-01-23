bfb4d265999126ef79280a588fa7c7795e6d56f9
Shae <a.thomas.vc@gmail.com>
2024-01-23 10:32:39 -0800

    Made `update_manifest()` (optionally) test the new manifest with `crypt.migrate()`

--- frameworks/manifest_new/upstream/manifest.py
+++ frameworks/manifest_new/upstream/manifest.py
@@ -4,6 +4,7 @@
 import typing
 
 from ..file import extract
+from ..crypt import migrate
 from ..base import Manifest, ManifestType
 from ..exceptions import *
 
@@ -19,10 +20,16 @@ def fetch_manifest(url: str, type_: None | ManifestType = None, *, fetch: typing
         if (type_ is None) or (type_.type == m.type): return m
         raise ManifestTypeError(f'{url} is of an unexpected type (expected {type_.type}, got {m.type})')
     raise CorruptedFileException(f'File fetched from {url!r} is not a manifest')
-def update_manifest(m: Manifest) -> Manifest:
-    '''Fetches the manifest from `m`'s upstream (under the `metadata` part) and returns it'''
+def update_manifest(m: Manifest, migrate_test: bool = True) -> Manifest:
+    '''
+        Fetches the manifest from `m`'s upstream (under the `metadata` part) and returns it
+
+        Uses `crypt.migrate()` to test the new manifest against the old if `migrate_test` is true
+    '''
     if not hasattr(m, 'metadata'):
         raise MissingPartError('Cannot ascertain manifest\'s upstream when it does not have a "metadata" part')
     if getattr(m.metadata, 'upstream', None) is None:
         raise PartTypeError('Manifest\'s metadata.upstream value is either missing or None')
-    return fetch_manifest(m.metadata.upstream, type(m))
+    new = fetch_manifest(m.metadata.upstream, type(m))
+    if migrate_test: migrate(new, m)
+    return new
