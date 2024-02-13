bd540ebed5aa5b4ee4d90f144fc70ad51c1260e0
Shae <a.thomas.vc@gmail.com>
2024-02-13 15:12:07 -0800

    Implemented basic features of `Config`

--- util/data/__init__.py
+++ util/data/__init__.py
@@ -3,6 +3,7 @@
 '''Libraries for data processing and storage'''
 
 #> Package >/
-__all__ = ('base85',)
+__all__ = ('base85', 'config')
 
+from . import config
 from .._general import base85
---
+++ util/data/config.py
@@ -0,0 +1,63 @@
+#!/bin/python3
+
+'''
+    Provides very basic JSON-based configuration for FlexiLynx, in a not-super-readable format
+        It is recommended to suggest that end-users install a module that provides configuration options
+'''
+
+#> Imports
+import json
+import typing
+from pathlib import Path
+from threading import RLock
+from collections import UserDict
+
+from ..parallel import mlock
+#</Imports
+
+#> Header >/
+class Config(UserDict):
+    __slots__ = ('data', 'path', 'defaults', '_lock')
+
+    def __init__(self, path: Path):
+        self.data = {}
+        self.path = path
+        path.parent.mkdir(exist_ok=True, parents=True)
+        self.defaults = set()
+        self._lock = RLock()
+
+    @mlock
+    def __setitem__(self, item: str, val: typing.Any):
+        if item in self.defaults:
+            self.defaults.remove(item)
+        self.data[item] = val
+    @mlock
+    def __delitem__(self, item: str):
+        if item in self.defaults:
+            self.defaults.remove(item)
+        super().__delitem__(item)
+    @mlock
+    def get(self, item: str, default: typing.Any = None, insert_default: bool = True) -> typing.Any:
+        '''
+            Gets an item, returning `default` if its not set
+                If `insert_default` is true, and the item is not set, then runs `.default(item, default)`
+        '''
+        if item not in self.data:
+            if insert_default:
+                self.default(item, default)
+            return default
+        val = self.data[item]
+        if (item in self.defaults) and (val != default):
+            val = default
+            if insert_default: self.default(item, default)
+        return val
+    @mlock
+    def default(self, item: str, val: typing.Any, override: bool = True):
+        '''
+            Sets a default for an item and marks it as such
+                Refuses to override a non-default value without `override`
+        '''
+        if (item in self.data) and (item not in self.defaults) and not override:
+            raise KeyError(f'Cannot set default for existing non-default {item!r} without override')
+        self.data[item] = val
+        self.defaults.add(item)
