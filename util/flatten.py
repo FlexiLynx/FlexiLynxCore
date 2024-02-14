dc334092b2078d5c79cbbe1520b142b0ae283f61
Shae <a.thomas.vc@gmail.com>
2024-02-14 08:47:01 -0800

    Implemented `util.flatten.extrude_map()`

--- util/flatten.py
+++ util/flatten.py
@@ -4,11 +4,13 @@
 
 #> Imports
 import typing
+import itertools
 #</Imports
 
 #> Header >/
-__all__ = ('flatten_map',)
+__all__ = ('flatten_map', 'extrude_map')
 
+# Maps
 def flatten_map(m: typing.Mapping[str, typing.Any], delim: str | None = None) -> dict[tuple[str, ...] | str, typing.Any]:
     '''
         Flattens `m` into a dictionary with no depth
@@ -25,3 +27,16 @@ def flatten_map(m: typing.Mapping[str, typing.Any], delim: str | None = None) ->
     if delim is not None:
         return {delim.join(k): v for k,v in new.items()}
     return new
+def extrude_map(m: typing.Mapping[tuple[str, ...] | str, typing.Any], delim: str | None = None) -> dict[str, typing.Any]:
+    '''Reverses the effects of `flatten_map()`'''
+    new = {}
+    for (l,k),_ in itertools.groupby(m.keys(), key=lambda k: ((len(k) if delim is None else k.count(delim)+1), k)):
+        assert l
+        ks = tuple(k if delim is None else k.split(delim))
+        if l == 1:
+            new[ks[0]] = m[k]
+            continue
+        if ks[0] not in new:
+            new[ks[0]] = {}
+        new[ks[0]][ks[1:]] = m[k]
+    return {k: (extrude_map(v) if isinstance(v, dict) else v) for k,v in new.items()}
