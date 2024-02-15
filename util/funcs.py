545c0ba0154dc4a500ee5eba2cef0597788812d0
Shae <a.thomas.vc@gmail.com>
2024-02-15 11:31:53 -0800

    Implemented `reach()` frame utility function; moved lambda~~s~~ to `lambdas` namespace

--- util/funcs.py
+++ util/funcs.py
@@ -1,14 +1,29 @@
 #!/bin/python3
 
-'''A collection of functions that were commonly created as lambdas'''
+'''
+    A collection of function and frame utilities,
+        as well as some functions that are commonly created as lambdas
+'''
 
 #> Imports
-
+import types
+import inspect
 #</Imports
 
 #> Header >/
-__all__ = ('noop',)
+__all__ = ('lambdas', 'reach')
 
-def noop(*args, **kwargs) -> None:
-    '''Takes everything and returns nothing (`None`)'''
-    return None
+lambdas = types.SimpleNamespace(
+    noop = lambda *args, **kwargs: None,
+)
+
+def reach(n: int = 1, current: types.FrameType | None = None) -> types.FrameType:
+    '''
+        Reaches "up" `n` frames from `current`
+        If `current` is `None`, the caller's frame is used instead
+    '''
+    if current is None:
+        current = inspect.currentframe().f_back
+    for _ in range(n):
+        current = current.f_back
+    return current
