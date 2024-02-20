ef2c5755d8eaba4cd183afb6bd44096c64c85ad7
Shae <a.thomas.vc@gmail.com>
2024-02-20 12:09:09 -0800

    Temporarily removed `sync`

--- frameworks/blueprint/__init__.py
+++ frameworks/blueprint/__init__.py
@@ -3,8 +3,7 @@
 '''FlexiLynx's `blueprint` framework'''
 
 #> Package >/
-__all__ = ('Blueprint', 'BlueProtocol', 'parts', 'sync')
+__all__ = ('Blueprint', 'BlueProtocol', 'parts')
 
 from . import parts
-from .sync import sync
 from .blueprint import Blueprint, BlueProtocol
