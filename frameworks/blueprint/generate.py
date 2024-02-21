e15254fd2b2c5922f72bf0b9ee816df535213ab5
Shae <a.thomas.vc@gmail.com>
2024-02-21 09:36:21 -0800

    Implemented `hashtools` usage in `hash_files()`

--- frameworks/blueprint/generate.py
+++ frameworks/blueprint/generate.py
@@ -10,21 +10,20 @@ from pathlib import Path
 
 from . import parts
 from .blueprint import Blueprint
+
+from FlexiLynx.core.util import hashtools
 #</Imports
 
 #> Header >/
 __all__ = ('hash_files', 'make_manifest')
 
-def hash_files(root: Path, files: typing.Sequence[str], *, max_processes: int = 8, hash_method: str = 'sha1') -> dict[str, bytes]:
+def hash_files(root: Path, files: typing.Sequence[str], *, max_threads: int = 8, hash_method: str = 'sha1') -> dict[str, bytes]:
     '''Hashes a set of files, prepending them with `root`'''
-    procs = min(len(files), max_processes)
-    hfunc = lambda f: (f, hashlib.new(hash_method, (root / f).read_bytes()).digest())
-    if procs < 2: return dict(map(hfunc, files))
-    with multiprocessing.pool.ThreadPool(procs) as mp:
-        return dict(mp.map(hfunc, files))
+    paths = {root/f: f for f in files}
+    return {paths[p]: h for p,h in hashtools.hash_files(*paths.keys(), max_threads=max_threads, hash_method=hash_method).items()}
 
-def make_manifest(url: str | None, *files: Path, root: Path = Path('.'), max_processes: int = 8, hash_method: str = 'sha1') -> parts.Manifest:
+def make_manifest(url: str | None, *files: Path, root: Path = Path('.'), max_threads: int = 8, hash_method: str = 'sha1') -> parts.Manifest:
     '''Creates a manifest from a set of files, relative to `root`'''
     return parts.Manifest(url=url, hash_method=hash_method,
                           files=hash_files(root, tuple((f.relative_to(root).as_posix() for f in files)),
-                                           max_processes=max_processes, hash_method=hash_method))
+                                           max_threads=max_threads, hash_method=hash_method))
