1041334a13d72902152565894db42d0e8a4838cf
Shae <a.thomas.vc@gmail.com>
2024-02-21 10:11:56 -0800

    Removed outdated scripts and tools

--- Tools/scripts/autoupdate_FlexiLynxCore_manifest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# when will Arch update core/Python?
-PYTHON=python3.12 ./Tools/scripts/update_FlexiLynxCore_manifest.sh \
-    && (cd ../ManifestStore; git add flexilynx/core.ini && git commit -m 'Automatically updated from commit in FlexiLynxCore' && git push)
--- Tools/scripts/update_FlexiLynxCore_manifest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-${PYTHON:-} ./Tools/manifestutils.py mod update ../ManifestStore/flexilynx/core.ini \
-    --default-root . --include '**/*.py' --exclude 'Tools/*' \
-    --sign ../shae.pyk \
-    --meta-version "commit $(git rev-parse --short HEAD)"
