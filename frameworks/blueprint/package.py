b08b66f7f4184d1766577febe63cf724a97e3a29
Shae <a.thomas.vc@gmail.com>
2024-02-20 17:36:09 -0800

    Implemented `Package.uninstall()`

--- frameworks/blueprint/package.py
+++ frameworks/blueprint/package.py
@@ -8,10 +8,14 @@ from pathlib import Path
 
 from .generate import hash_files
 from .blueprint import Blueprint
+
+import FlexiLynx
 #</Imports
 
 #> Header >/
 __all__ = ('Package',)
+
+logger = FlexiLynx.core.logger.getChild('BP')
 
 class Package:
     '''Represents a package that may or may not be on the system; built from a `Blueprint`'''
@@ -39,3 +43,32 @@ class Package:
         mismatching = {f for f,h in hash_files(at, files, max_processes=max_processes, hash_method=draft.hash_method).items() if draft.files[f] != h}
         # Return
         return (files - mismatching, mismatching, missing)
+    def uninstall(self, draft: str | None = None, *, at: Path = Path('.')):
+        '''Uninstalls files on the system, renaming any modified files to protect against accidental deletion'''
+        match,mism,miss = self.scan(draft, at=at)
+        match = sorted(match); mism = sorted(mism); miss = sorted(miss)
+        logger.info(f'Uninstall issued: {self.blueprint.id}{"" if draft is None else f"@{draft}"}')
+        if not (miss or mism or match):
+            logger.error('Does not appear to be installed? Skipping')
+            return
+        if miss: logger.error(f'Cannot uninstall missing files:\n - {"\n - ".join(miss)}')
+        if mism: logger.warning(f'Changed files will not be removed, but will be renamed:\n - {"\n - ".join(mism)}')
+        if match: logger.warning(f'Files will be removed:\n - {"\n - ".join(match)}')
+        for p in map(at.__truediv__, mism):
+            logger.verbose(f'Moving {p} -> {p}.old')
+            p.with_suffix(f'{p.suffix}.old').write_bytes(p.read_bytes())
+            logger.trace(f'Unlinking {p}')
+            p.unlink()
+        clean = set()
+        for p in map(at.__truediv__, match):
+            logger.verbose(f'Unlinking {p}')
+            p.unlink()
+            clean.add(p.parent)
+        # Clean missing paths
+        for p in sorted((p for p in clean), key=lambda p: (len(p.parts), p), reverse=True):
+            # If it has anything under it, then continue; otherwise, rmdir it!
+            try: next(p.iterdir())
+            except StopIteration: pass
+            else: continue
+            p.rmdir()
+            logger.verbose('Removed empty directory {p}')
