ebbdcd2eb640285a2ce1c5d3d91e1140f0644937
Shae <a.thomas.vc@gmail.com>
2024-02-21 10:33:51 -0800

    Fixed automatic `Blueprints` store updating script not pushing to origin

--- Tools/scripts/autoupdate_core_blueprint_store.sh
+++ Tools/scripts/autoupdate_core_blueprint_store.sh
@@ -2,4 +2,4 @@
 
 BP=`cat ../Blueprints/flexilynx/core.json | ./Tools/scripts/updblueprint.sh`
 echo "$BP" > ../Blueprints/flexilynx/core.json #\
-    #&& (cd ../Blueprints/; git add flexilynx/core.json && git commit -m 'Automatically updated from commit in FlexiLynxCore' && git push)
+    && (cd ../Blueprints/; git add flexilynx/core.json && git commit -m 'Automatically updated from commit in FlexiLynxCore' && git push)
