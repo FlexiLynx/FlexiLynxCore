bfd923df3b1f9cb55f536f35436640840e673acf
Shae <a.thomas.vc@gmail.com>
2024-02-22 10:27:55 -0800

    Created `functools.defaults()` decorator

--- util/tools/functools.py
+++ util/tools/functools.py
@@ -14,7 +14,7 @@ from .. import logger
 #</Imports
 
 #> Header >/
-__all__ = ('lambdas', 'reach')
+__all__ = ('lambdas', 'reach', 'DEFAULT', 'defaults')
 
 lambdas = types.SimpleNamespace(
     noop = lambda *args, **kwargs: None,
@@ -34,3 +34,27 @@ def reach(n: int = 1, current: types.FrameType | None = None) -> types.FrameType
         current = current.f_back
     rlogger.trace(f'reached up {n} frames to {current.f_globals["__name__"]!r} at {current.f_code.co_filename}@L{f.f_lineno}')
     return current
+
+class DEFAULT:
+    def __new__(cls):
+        return DEFAULT
+    def __reduce__(self):
+        return (type(self), ())
+DEFAULT = object.__new__(DEFAULT)
+def defaults(supplier: typing.Callable):
+    '''
+        Decorator to make a function automatically take default
+            values from another
+        Any default value of `DEFAULT` in the wrapped function is replaced with
+            the corresponding default from `supplier`
+        Note that this modifies the wrapped function in-place, it does not create a new one
+    '''
+    def mutator(target: typing.Callable) -> typing.Callable:
+        if target.__defaults__ and supplier.__defaults__:
+            target.__defaults__ = tuple((supplier.__defaults__[i] if d is DEFAULT else d) for i,d in enumerate(target.__defaults__))
+        if target.__kwdefaults__ and supplier.__kwdefaults__:
+            for k,v in target.__kwdefaults__.items():
+                if v is not DEFAULT: continue
+                target.__kwdefaults__[k] = supplier.__kwdefaults__[k]
+        return target
+    return mutator
