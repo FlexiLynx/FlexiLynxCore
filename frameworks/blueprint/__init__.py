54abc31c517437d08b9e138407ca90f9945543b6
Shae <a.thomas.vc@gmail.com>
2024-02-27 12:04:02 -0800

    Implemented `BasePackage`

--- frameworks/blueprint/__init__.py
+++ frameworks/blueprint/__init__.py
@@ -3,7 +3,7 @@
 '''FlexiLynx's `blueprint` framework'''
 
 #> Package >/
-__all__ = ('DEFAULT_HASH_ALGORITHM', 'Blueprint', 'BlueProtocol', 'BaseManagedPackage', 'Package', 'crypt', 'generate', 'parts')
+__all__ = ('DEFAULT_HASH_ALGORITHM', 'Blueprint', 'BlueProtocol', 'crypt', 'generate', 'package', 'parts')
 
 DEFAULT_HASH_ALGORITHM = 'sha3_384' # reasonably secure and forward-proof, but not too long (60 chars in Base85)
 
@@ -11,7 +11,7 @@ from FlexiLynx.core import logger
 logger = logger.getChild('BP')
 
 from .blueprint import Blueprint, BlueProtocol
-from .package import BaseManagedPackage, Package
 from . import crypt
-from . import parts
 from . import generate
+from . import package
+from . import parts
--- frameworks/blueprint/package.py
+++ frameworks/blueprint/package.py
@@ -3,7 +3,36 @@
 '''Supplies classes for packages'''
 
 #> Imports
+import typing
+
+from .blueprint import Blueprint
+
+from . import logger
+
+from FlexiLynx.core.util.functools import defaults, DEFAULT
 #</Imports
 
 #> Header >/
-__all__ = ()
+__all__ = ('BasePackage', 'ManagedPackage')
+
+class BasePackage:
+    '''
+        Represents an abstract package that may or may not be on the system
+        Mostly a container for a `Blueprint`
+    '''
+    __slots__ = ('blueprint',)
+
+    def __init__(self, blueprint: Blueprint):
+        self.blueprint = blueprint
+
+    @defaults(Blueprint.update)
+    def update(self, url: str | None = DEFAULT, *, fetchfn: typing.Callable[[str], bytes] = DEFAULT,
+               verify: bool = DEFAULT, verify_self: bool = DEFAULT, key_update: Blueprint.KeyUpdate = DEFAULT):
+        '''
+            Updates this package's underlying blueprint
+            See `help(Blueprint.update)` for information on arguments
+        '''
+        logger.debug('Issuing update to blueprint through package')
+        logger.trace(f'{url=!r} {fetchfn=!r} {verify=!r} {verify_self=!r} {key_update=!r}')
+
+class ManagedPackage: pass
