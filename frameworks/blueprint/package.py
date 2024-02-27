74f65801d66b69c7b9d6d78b0307f3205c58ebd3
Shae <a.thomas.vc@gmail.com>
2024-02-27 11:17:19 -0800

    Mostly implemented `BaseManagedPackage`

--- frameworks/blueprint/package.py
+++ frameworks/blueprint/package.py
@@ -3,22 +3,26 @@
 '''Supplies the `Package` class'''
 
 #> Imports
+import json
 import typing
+import functools
+import threading
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from .generate import hash_files
 from .blueprint import Blueprint
 
 from . import logger
 
-import FlexiLynx
+from FlexiLynx.core.util import Config
 from FlexiLynx.core.util import hashtools
 from FlexiLynx.core.util.net import fetchx
 from FlexiLynx.core.util.functools import defaults, DEFAULT
 #</Imports
 
 #> Header >/
-__all__ = ('Package',)
+__all__ = ('Package', 'BaseManagedPackage')
 
 class Package:
     '''Represents a package that may or may not be on the system; built from a `Blueprint`'''
@@ -144,3 +148,122 @@ class Package:
                         logger.verbose(f'Removed empty directory {p}')
                     p = p.parent
                     if p in clean: break
+
+class BaseManagedPackage(Package):
+    '''...'''
+    __slots__ = ('install_location', 'config', 'drafts', 'files', 'keep', '_save_thread', '_save_condition')
+
+    def __init__(self, install_location: Path):
+        super().__init__(Blueprint.deserialize((install_location/'blueprint.json').read_text()))
+        self.config = Config(install_location/'config.json')
+        self.config.load(self.config.path.read_text() if self.config.path.exists() else '{}')
+        self.drafts = self.config.get('drafts', [])
+        self.files = self.config.get('files', ['blueprint.json', 'config.json'])
+        self.keep = self.config.get('keep', ['blueprint.json', 'config.json'])
+        self._save_condition = threading.Condition()
+        self._save_thread = threading.Thread(target=self._save_loop, daemon=True)
+    def _save_loop(self):
+        with self._save_condition:
+            while True:
+                self._save_condition.wait()
+                self.config.save()
+
+    def selected_files(self) -> typing.Iterator[str]:
+        '''
+            Yields all selected files, sourcing from all enabled drafts
+            *Will* yield copies if they exist in drafts
+        '''
+        yield from iter(self.blueprint.main.files.keys())
+        if not self.drafts:
+            return
+        if self.blueprint.drafts is None:
+            raise TypeError(f'Cannot select draft {draft!r}, as the blueprint does not support drafts')
+        for d in self.drafts:
+            yield from iter(self.blueprint.drafts[d].files.keys())
+    def upgrade(self):
+        '''
+            Installs the package in a temporary location, then copies it over,
+                additionally removing registered files that are no longer needed
+            Drafts, if added, are installed in the order of the `drafts` field
+        '''
+        logger.trace(f'Upgrade issued for: {self.blueprint.id}')
+        cfiles = frozenset(self.files)
+        nfiles = frozenset(self.selected_files())
+        _to_keep = frozenset(self.keep)
+        to_install = (nfiles - cfiles) - _to_keep
+        to_remove = (cfiles - nfiles) - _to_keep
+        to_replace = (cfiles & nfiles) - _to_keep
+        if to_install: logger.terse(f'The following files are selected for installation:\n - {"\n - ".join(sorted(to_install))}')
+        if to_replace: logger.warning(f'The following files are selected for removal:\n - {"\n - ".join(sorted(to_remove))}')
+        if to_remove: logger.warning(f'The following files are selected for (potential) replacement:\n - {"\n - ".join(sorted(to_replace))}')
+        if not (to_install or to_replace or to_remove):
+            logger.warning('Nothing to do, upgrade cancelled')
+            return
+        if to_install or to_replace:
+            with TemporaryDirectory() as tmploc:
+                tmploc = Path(tmploc)
+                logger.verbose(f'Copying registered files that are selected for potential replacement to {tmploc}')
+                for f in to_replace:
+                    (tmploc/f).parent.mkdir(parents=True, exist_ok=True)
+                    (tmploc/f).write_text((self.install_location/f).read_text())
+                logger.verbose(f'Installing upgrade for main and {len(self.drafts)} draft(s) to temporary location: {tmploc}')
+                self.install(at=tmploc)
+                for d in self.drafts:
+                    self.install(d, at=tmploc)
+                logger.info(f'Upgrade downloaded for: {self.blueprint.id}; clearing current files')
+                if to_install:
+                    logger.info(f'Copying {len(to_install)} file(s) selected for installation')
+                    for f in to_install:
+                        logger.verbose(f'Installing {tmploc/f} to {self.install_location/f}')
+                        if (self.install_location/f).exists():
+                            logger.error(f'File registered as not installed, yet is present on the filesystem: (self.install_location/f)\nRefusing to install')
+                            continue
+                        logger.trace(f'Copied {(self.install_location/f).write_bytes((tmploc/f).read_bytes())} byte(s) from {tmploc/f} to {self.install_location/f}')
+                if to_replace:
+                    logger.warning(f'Copying {len(to_replace)} file(s) as replacements')
+                    for f in to_replace:
+                        logger.terse(f'Replacing {self.install_location/f} with {tmploc/f}')
+                        logger.trace(f'Copied {(self.install_location/f).write_bytes((tmploc/f).read_bytes())} byte(s) from {tmploc/f} to {self.install_location/f}')
+        elif not to_remove:
+            logger.warning('Nothing to do, upgrade cancelled')
+            return
+        if to_remove:
+            logger.warning(f'Removing {len(to_remove)} file(s)')
+            for f in to_remove:
+                logger.warning(f'Unlinking {self.install_location/f}')
+                (self.install_location/f).unlink()
+            logger.info('Updating files database')
+        self.files.clear()
+        self.files.extend(sorted(to_install + to_replace))
+
+    def sync(self, **update_kwargs):
+        '''
+            ...
+            See `help(Package.update)` for `update_kwargs`
+        '''
+        logger.trace(f'Sync issued for: {self.blueprint.id}')
+        self.config.save()
+        self.update(**update_kwargs)
+        self.upgrade()
+        self.config.save()
+    def add_draft(self, draft: str):
+        '''
+            Adds a draft to the configured drafts
+            Does nothing if `draft` is already in the drafts list
+            Drafts are installed on the next call to `sync()`/`upgrade()`
+        '''
+        if 'drafts' in self.config['drafts']: return
+        if self.blueprint.drafts is None:
+            raise TypeError(f'Cannot add draft {draft!r}, as the blueprint does not support drafts')
+        if draft not in self.blueprint.drafts:
+            raise ValueError(f'Cannot add draft {draft!r}, as it does not exist in the blueprint')
+        self.config['drafts'].append(draft)
+        with self._save_condition: self._save_condition.notify()
+    def rm_draft(self, draft: str):
+        '''
+            Removes a draft from the configured drafts
+            Does nothing if `draft` is not in the drafts list
+            Drafts are removed on the next call to `sync()`/`upgrade()`
+        '''
+        try: self.config['drafts'].remove(draft)
+        except ValueError: pass
