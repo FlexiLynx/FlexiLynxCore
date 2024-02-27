0771c3ea86e8cd193ed0f92ff2b578db779ef43f
Shae <a.thomas.vc@gmail.com>
2024-02-27 14:57:38 -0800

    Implemented `util.fstools.clean_empty()`

--- util/tools/fstools.py
+++ util/tools/fstools.py
@@ -7,7 +7,7 @@ from pathlib import Path
 #</Imports
 
 #> Header >/
-__all__ = ('clean_pycache',)
+__all__ = ('clean_pycache', 'clean_empty')
 
 def clean_pycache(root: Path):
     '''
@@ -18,3 +18,12 @@ def clean_pycache(root: Path):
         for pycf in pycd.glob('*.pyc'): pycf.unlink()
         try: pycd.rmdir()
         except OSError: pass
+def clean_empty(root: Path, include_root: bool = False):
+    '''
+        Recursively rmdirs empty directories,
+            not including `root` unless `include_root`
+    '''
+    for rp,ds,fs in root.walk(top_down=False):
+        if fs: continue
+        if not any(map(Path.exists, map(rp.joinpath, ds))):
+            rp.rmdir()
