1df2cc8422142ef57fb2789f251dc586ecd76350
Shae <a.thomas.vc@gmail.com>
2024-02-29 11:08:59 -0800

    Implemented `module.loader.import_module()`

--- frameworks/module/__init__.py
+++ frameworks/module/__init__.py
@@ -2,8 +2,18 @@
 
 '''FlexiLynx's `module` framework'''
 
+#> Imports
+from types import SimpleNamespace
+from typing import Callable
+#</Imports
+
 #> Package >/
-__all__ = ('Module', 'loader')
+__all__ = ('Consts', 'Module', 'loader')
+
+Consts = SimpleNamespace(
+    ENTRYPOINT_FILE='__entrypoint__.py',
+    THIS_NAME='this',
+)
 
 from .module import Module
 from . import loader
--- frameworks/module/loader.py
+++ frameworks/module/loader.py
@@ -4,16 +4,20 @@
 
 #> Imports
 import json
+import typing
+from types import ModuleType
 from pathlib import Path
 
 from .module import Module
+
+from . import Consts
 
 from FlexiLynx.core.util.moduletools import inject_import
 from FlexiLynx.core.frameworks.blueprint import package
 #</Imports
 
 #> Header >/
-__all__ = ('load_metadata', 'create_module', 'module_from_package')
+__all__ = ('load_metadata', 'create_module', 'module_from_package', 'import_module')
 
 def load_metadata(path: Path) -> dict:
     '''Loads module metadata from `path`'''
@@ -25,3 +29,11 @@ def module_from_package(pack: package.TPackage(installed=True, not_installed=Fal
     '''Creates a `Module` from a package'''
     meta = load_metadata(pack.at)
     return Module(type=meta['type'], metadata=meta, package=pack)
+def import_module(mod: Module, extra_inject: dict[str, typing.Any] = {}) -> ModuleType:
+    '''
+        Given a `Module` (FlexiLynx module), import and return its **Python** module
+        Note that this also injects `mod` as `Consts.THIS_NAME` ("this" by default)
+            into the module's namespace before importing it, as well as any names in `extra_inject`
+    '''
+    return inject_import(mod.package.at/Consts.ENTRYPOINT_FILE, mod.id,
+                         **({Consts.THIS_NAME: mod} | extra_inject))
