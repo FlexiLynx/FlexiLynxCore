cef80dfc67d2dd5300472b544644dea1d6816070
Shae <a.thomas.vc@gmail.com>
2024-03-01 13:19:29 -0800

    Cleaned up __entrypoint__

--- __entrypoint__.py
+++ __entrypoint__.py
@@ -2,43 +2,43 @@
 
 #> Imports
 import sys
-import typing
 import tomllib
-import logging
-import importlib
-from types import ModuleType
+from pathlib import Path
+from importlib import import_module
 #</Imports
 
 #> Header
-MIN_PYTHON_VERSION = (3, 12, 0)
 FlexiLynx = NotImplemented
-def _resolve_import(modname: str, *, fn: typing.Callable = importlib.import_module):
-    if not __package__:
-        return fn(modname)
-    return fn(f'.{modname}', package=__package__)
+
+MIN_PYTHON_VERSION = (3, 12, 0)
+
+def _import(name):
+    return import_module(f'.{name}' if __package__ else name, package=__package__)
+#</Header
+
+#> Main >/
 def __load__():
     '''Load core Python modules'''
     # Check for minimum version
     assert sys.version_info[:3] >= MIN_PYTHON_VERSION, f'Minimum Python version not met! Need {".".join(map(str, MIN_PYTHON_VERSION))}, got {".".join(map(str, sys.version_info[:3]))}'
     # Import util
-    util = _resolve_import('util')
+    util = _import('util')
     # Create FlexiLynx FlexiSpace
     global FlexiLynx
     FlexiLynx = util.FlexiSpace('FlexiLynx', 'The shared library across FlexiLynx')
     # Add utils
     (FlexiLynx@'core').util = util
-def __setup__():
+def __setup__(log_cfg: Path = Path('logging.toml')):
     # Setup logger
-    with open('logging.toml', 'rb') as f:
-        FlexiLynx.core.util.logger.init(tomllib.load(f))
+    FlexiLynx.core.util.logger.init(tomllib.loads(log_cfg.read_text())
+                                    if log_cfg.exists() else {})
     FlexiLynx.logger = FlexiLynx.core.util.logger.root_logger
     FlexiLynx.core.logger = FlexiLynx.core.util.logger.core_logger
     # Add frameworks
     FlexiLynx.core@'frameworks'
-    FlexiLynx.core.frameworks.blueprint = _resolve_import('frameworks.blueprint')
-    FlexiLynx.core.frameworks.module = _resolve_import('frameworks.module')
-    FlexiLynx.core.frameworks.plugins = _resolve_import('frameworks.plugin')
-#</Header
+    FlexiLynx.core.frameworks.blueprint = _import('frameworks.blueprint')
+    FlexiLynx.core.frameworks.module = _import('frameworks.module')
+    FlexiLynx.core.frameworks.plugins = _import('frameworks.plugin')
 
-#> Main >/
-if __name__ == '__main__': raise NotImplementedError
+if __name__ == '__main__':
+    raise NotImplementedError('The __entrypoint__ should not be executed directly')
