368e6a4bcfcb07b0e46c5ff17702cbaa1b68a3c3
Shae <a.thomas.vc@gmail.com>
2024-03-18 12:06:24 -0700

    Remove `VERSION`

--- __entrypoint__.py
+++ __entrypoint__.py
@@ -1,1 +1,56 @@
-#!/bin/python3 VERSION = '8ed397b')
+#!/bin/python3
+
+#> Imports
+import sys
+import tomllib
+from pathlib import Path
+from importlib import import_module
+#</Imports
+
+#> Header
+__all__ = ('FlexiLynx', 'MIN_PYTHON_VERSION', '__load__', '__setup__')
+
+FlexiLynx = NotImplemented
+
+MIN_PYTHON_VERSION = (3, 12, 0)
+
+def _import(name):
+    return import_module(f'.{name}' if __package__ else name, package=__package__)
+#</Header
+
+#> Main >/
+def __load__():
+    '''Load core Python modules'''
+    # Check for minimum version
+    assert sys.version_info[:3] >= MIN_PYTHON_VERSION, f'Minimum Python version not met! Need {".".join(map(str, MIN_PYTHON_VERSION))}, got {".".join(map(str, sys.version_info[:3]))}'
+    # Import util
+    util = _import('util')
+    # Create FlexiLynx FlexiSpace
+    global FlexiLynx
+    FlexiLynx = util.FlexiSpace('FlexiLynx', 'The shared library across FlexiLynx')
+    # Add utils
+    (FlexiLynx@'core').util = util
+def __setup__(log_cfg: Path = Path('logging.toml')):
+    '''
+        Setup and load logging and frameworks features:
+        - Setup logger
+        - Load `blueprint`, `module`, and `plugin` frameworks
+        - Initialize `module` manager
+        - Initialize `plugin` manager and bind it to `module` manager
+    '''
+    # Setup logger
+    FlexiLynx.core.util.logger.init(tomllib.loads(log_cfg.read_text())
+                                    if log_cfg.exists() else {})
+    FlexiLynx.logger = FlexiLynx.core.util.logger.root_logger
+    FlexiLynx.core.logger = FlexiLynx.core.util.logger.core_logger
+    # Add frameworks
+    FlexiLynx.core@'frameworks'
+    FlexiLynx.core.frameworks.blueprint = _import('frameworks.blueprint')
+    FlexiLynx.core.frameworks.module = _import('frameworks.module')
+    FlexiLynx.core.frameworks.plugin = _import('frameworks.plugin')
+    # Initialize managers
+    FlexiLynx.modules = FlexiLynx.core.frameworks.module.Manager()
+    FlexiLynx.plugins = FlexiLynx.core.frameworks.plugin.Manager(FlexiLynx.modules)
+
+if __name__ == '__main__':
+    raise NotImplementedError('The __entrypoint__ should not be executed directly')
